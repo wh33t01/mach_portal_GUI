3b1f8e5fb4f9dcec876deee22c47b9d9d6d0d004
BocuD <duco.born@gmail.com>
2016-12-18 11:24:51 +0100

    Added support for 34 devices

--- mach_portal/mach_portal/offsets.c
+++ mach_portal/mach_portal/offsets.c
@@ -33,8 +33,111 @@ uint64_t struct_ipc_space_is_table_offset;
 // offsets in struct ipc_port
 uint64_t struct_ipc_port_ip_kobject_offset;
 
+//file:kernel_iPhone8,2_10.1.1_14B150_n56.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_T7000
 
-void init_iphone_6s_10_1_1_14b150(){
+//file:kernel_iPhone8,1_10.1.1_14B150_n61.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_T7000
+
+void init_iphone_6s_plus_and_iphone_6s_n61() {
+    allproc_offset = 0x5b4168;
+    kernproc_offset = 0x5ba0e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
+
+//file:kernel_iPhone8,4_10.1.1_14B100_n69.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_S8000
+void init_iphone_se() {
+    allproc_offset = 0x5a4148;
+    kernproc_offset = 0x5aa0e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
+
+//file:kernel_iPhone9,1_10.1.1_14B150_d10.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_T8010
+void init_iphone_7() {
+    allproc_offset = 0x5ec178;
+    kernproc_offset = 0x5f20e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
+
+//file:kernel_iPhone9,4_10.1.1_14B100_d11.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_T8010
+void init_iphone_7_plus() {
+    allproc_offset = 0x5ec178;
+    kernproc_offset = 0x5f20e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
+
+void init_iphone_6s_n71(){
   printf("setting offsets for iPhone 6s 10.1.1\n");
   allproc_offset = 0x5A4148;            //allproc offset
   kernproc_offset = 0x5AA0E0;            //kernproc offset
@@ -57,7 +160,55 @@ void init_iphone_6s_10_1_1_14b150(){
     
   struct_ipc_port_ip_kobject_offset = 0x68;
 }
-void init_ipad_mini_2_10_1_1_14b100() {
+//file:kernel_iPhone7,1_10.1.1_14B100_n56.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_T7000
+void init_iphone_6_n56() {
+    allproc_offset = 0x5b4168;
+    kernproc_offset = 0x5ba0e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
+//file:kernel_iPhone7,1_10.1.1_14B100_n66.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_S8000
+void init_iphone_6_n66() {
+    allproc_offset = 0x5a4148;
+    kernproc_offset = 0x5aa0e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
+void init_ipad_mini_2() {
   printf("setting offsets for iPad mini 2 10.1.1\n");
   allproc_offset = 0x5A4128;
   kernproc_offset = 0x5AA0E0;
@@ -81,7 +232,7 @@ void init_ipad_mini_2_10_1_1_14b100() {
   struct_ipc_port_ip_kobject_offset = 0x68;
 }
 
-void init_ipod_touch_6g_10_1_1_14b100() {
+void init_ipod_touch_6g() {
   printf("setting offsets for iPod touch 6G 10.1.1\n");
   allproc_offset = 0x5B4168;
   kernproc_offset = 0x5BA0E0;
@@ -127,7 +278,30 @@ void init_macos_10_12_1() {
   
   struct_ipc_port_ip_kobject_offset = 0x68;
 }
-
+//file:kernel_iPhone7,2_10.1.1_14B100_n61.img.dec
+//kernel version:root:xnu-3789.22.3~1/RELEASE_ARM64_T7000
+void init_iphone_6_plus_n61() {
+    allproc_offset = 0x5b4168;
+    kernproc_offset = 0x5ba0e0;
+    
+    struct_proc_p_pid_offset = 0x10;
+    struct_proc_task_offset = 0x18;
+    struct_proc_p_uthlist_offset = 0x98;
+    struct_proc_p_ucred_offset = 0x100;
+    struct_proc_p_comm_offset = 0x26c;
+    
+    struct_kauth_cred_cr_ref_offset = 0x10;
+    
+    struct_uthread_uu_ucred_offset = 0x168;
+    struct_uthread_uu_list_offset = 0x170;
+    
+    struct_task_ref_count_offset = 0x10;
+    struct_task_itk_space_offset = 0x300;
+    
+    struct_ipc_space_is_table_offset = 0x20;
+    
+    struct_ipc_port_ip_kobject_offset = 0x68;
+}
 void unknown_build() {
   printf("This is an unknown kernel build - the offsets are likely to be incorrect and it's very unlikely this exploit will work\n");
   printf("You need to find these two kernel symbols:\n");
@@ -142,7 +316,7 @@ void init_offsets() {
   if (err == -1) {
     printf("uname failed - what platform is this?\n");
     printf("there's no way this will work, but trying anyway!\n");
-    init_ipad_mini_2_10_1_1_14b100();
+    init_ipad_mini_2();
     return;
   }
 
@@ -159,7 +333,7 @@ void init_offsets() {
     } else {
       unknown_build();
     }
-    init_ipod_touch_6g_10_1_1_14b100();
+    init_ipod_touch_6g();
     return;
   }
   if (strstr(u.machine, "iPad4,4")) {
@@ -169,21 +343,85 @@ void init_offsets() {
     } else {
       unknown_build();
     }
-    init_ipad_mini_2_10_1_1_14b100();
+    init_ipad_mini_2();
     return;
   }
-if (strstr(u.machine, "iPhone8,1")) {
+  if (strstr(u.machine, "iPhone8,1")) {
     // this is an iPhone 6S
     if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")){
         printf("this is a known kernel build for iPhone 6s - offsets should be okay\n");
     } else {
         unknown_build();
     }
-    init_iphone_6s_10_1_1_14b150();
+    init_iphone_6s_n71();
     return;
     }
-  
+  if (strstr(u.machine, "iPhone8,4")) {
+    // this is an iPhone se
+    if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")){
+        printf("this is a known kernel build for iPhone se - offsets should be okay\n");
+    } else {
+        unknown_build();
+    }
+    init_iphone_se();
+    return;
+    }
+  if (strstr(u.machine, "iPhone8,2")) {
+    // this is an iPhone 6s plus or 6s (n61)
+    if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")){
+        printf("this is a known kernel build for iPhone 6s plus or iPhone 6s (n61) - offsets should be okay\n");
+    } else {
+        unknown_build();
+    }
+    init_iphone_6s_plus_and_iphone_6s_n61();
+    return;
+    }
+  if (strstr(u.machine, "iPhone9,1")) {
+    // this is an iPhone 7
+    if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")){
+        printf("this is a known kernel build for iPhone 7 - offsets should be okay\n");
+    } else {
+        unknown_build();
+    }
+    init_iphone_7();
+    return;
+    }
+  if (strstr(u.machine, "iPhone9,4")) {
+    // this is an iPhone 7 plus
+    if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")){
+        printf("this is a known kernel build for iPhone 7 plus - offsets should be okay\n");
+    } else {
+        unknown_build();
+    }
+    init_iphone_7_plus();
+    return;
+    }
+  if (strstr(u.machine, "iPhone9,3")) {
+    // this is an iPhone 7 plus as well
+    if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T8010")){
+        printf("this is a known kernel build for iPhone 7 plus - offsets should be okay\n");
+    } else {
+        unknown_build();
+    }
+    init_iphone_7_plus();
+    return;
+    }
+  if (strstr(u.machine, "iPhone7,1")) {
+    // this is an iPhone 6
+    if (strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_S8000")){
+        printf("this is a known kernel build for iPhone 6 - offsets should be okay\n");
+        init_iphone_6_n66();
+    } else {
+        if(strstr(u.version, "root:xnu-3789.22.3~1/RELEASE_ARM64_T7000")){
+            printf("this is a known kernel build for iPhone 6 - offsets should be okay\n");
+            init_iphone_6_n56();
+        } else {
+            unknown_build();
+            return;
+        }
+    }
+  }
   printf("don't recognize this platform\n");
   unknown_build();
-  init_ipad_mini_2_10_1_1_14b100(); // this won't work!
+  init_ipad_mini_2(); // this won't work!
 }
